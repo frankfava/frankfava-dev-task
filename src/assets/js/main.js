75816b0e693bd7064d356eb987bb1b8d5d7b8d54
Frank Fava <frank.fava@igomoon.com>
2021-02-10 13:55:09 +0100

    Show Name field on email input

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,13 @@
-(function ($) { 
-	
+(function ($) {
+	if ($('.optin-form .hidden-fields').length) {
+		$('.optin-form .hidden-fields').each(function () {
+			var hidden = $(this);
+			var form = hidden.parents('form').first();
+			hidden.hide();
+			form.find('.input-email .form-control')
+				.one('keyup', function () {
+					hidden.slideDown();				
+				});
+		});
+	}
 })(jQuery);
