6fa5e6cc6aa575039ab3263f8d58e9c90efd6752
Frank Fava <frank.fava@igomoon.com>
2021-02-14 14:14:09 +0100

    Styling

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,10 +3,10 @@
 		$('.optin-form .hidden-fields').each(function () {
 			var hidden = $(this);
 			var form = hidden.parents('form').first();
-			hidden.hide();
-			form.find('.input-email .form-control')
+			hidden.removeClass('show');
+			form.find('.input-email input')
 				.one('keyup', function () {
-					hidden.slideDown();				
+					hidden.addClass('show');			
 				});
 		});
 	}
